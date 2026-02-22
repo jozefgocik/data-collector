fcc8e1c1ee51df9b06b213f22420df9070fecdb4
Jozef Gocik <106110686+jozefgocik@users.noreply.github.com>
2026-02-22 17:24:27 +0100

    Update currency.py

--- currency.py
+++ currency.py
@@ -2,26 +2,29 @@ import requests
 from datetime import datetime
 import csv
 
-URL = "https://api.exchangerate.host/latest?base=USD"
+# Using Frankfurter API (free, no key)
+URL = "https://api.frankfurter.app/latest?from=USD"
 
 response = requests.get(URL)
 data = response.json()
 
-date = data["date"]
-rates = data["rates"]
+# print(data)
+
+if "rates" not in data:
+    print("API Error:", data)
+    exit(1)
 
 today = datetime.utcnow().strftime("%Y-%m-%d")
 time = datetime.utcnow().strftime("%H:%M")
 
-# Write CSV
+base = data["base"]
+rates = data["rates"]
+
 with open("currency.csv", "a", newline="", encoding="utf-8") as f:
     writer = csv.writer(f)
 
-    # Header if file is new
-    # Uncomment if writing new file
-    writer.writerow(["date", "time", "base", "target", "rate"])
-
     for target, rate in rates.items():
-        writer.writerow([today, time, data["base"], target, rate])
+        writer.writerow([today, time, base, target, rate])
 
 print("Currency rates saved.")
+
